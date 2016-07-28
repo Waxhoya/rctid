602bff64328b48e4bd33ff53ae1d9b454d69fa0e
Waxhoya <waxhoya@gmail.com>
2016-07-27 21:02:30 -0700

    working random selector, upvote tally, and image cycling.

--- js/imgSelector.js
+++ js/imgSelector.js
@@ -1,28 +1,44 @@
-var picTwo = document.getElementById("secondPic");
-var picOne = document.getElementById("firstPic");
-var picThree= document.getElementById("thirdPic");
-var which = [0,1,2];
-
-picOne.addEventListener("click", scarfBox[which[0]].upvote)
-picTwo.addEventListener("click", scarfBox[which[1]].upvote)
-picThree.addEventListener("click", scarfBox[which[2]].upvote)
+var which = [];
 
 function imgSelector(array) {
 	var picDelivered = 3; // Change the array length of pictures returned
 	which = [];
 
-	for (i = 0; i < picDelivered-1; i++){
+	for (i = 1; i <= picDelivered; i++){
 		var picSelect = Math.floor(Math.random() * scarfBox.length);
-		console.log("Picture Chosen:"+picSelect)
-		if (which.indexOf(picSelect)){
+		console.log("Picture: "+picSelect+"selected")
+		if (which.indexOf(picSelect) >= 0){
+			console.log("Picture: "+picSelect+" was already Selected");
 			i -= 1;
 			continue;
 		} else {
-			which.push(picSelect);
+			var theSelection = array[picSelect].image;
+			console.log("Attempting to push "+theSelection+" to scarfPic"+i+"!");
+			which.push(theSelection);
+			document.getElementById("scarfPic"+i).src = theSelection;
 		}
 	}
-	picOne.src=""+(scarfBox[which[0]].image);
-	picTwo.src=""+(scarfBox[which[1]].image);
-	picThree.src=""+(scarfBox[which[2]].image);
+}
 
+document.getElementById("scarfPic1").addEventListener("click", upvoteClick);
+document.getElementById("scarfPic2").addEventListener("click", upvoteClick);
+document.getElementById("scarfPic3").addEventListener("click", upvoteClick);
+
+
+function upvoteClick(event) {
+	var imageSource = event.target;
+	console.log(imageSource);
+	var getSource = imageSource.src;
+	for (var index = 0; index < scarfBox.length; index++) {
+		console.log("is "+scarfBox[index].image+" = "+getSource);
+		if (getSource.indexOf(scarfBox[index].image) >= 0) {
+			console.log("found it: "+imageSource.src+" adding one to upvotes");
+			console.log(scarfBox[index].upVotes);
+			scarfBox[index].upVotes++;
+			console.log(scarfBox[index].upVotes);
+			imgSelector(scarfBox);
+		} else {
+		//console.log(scarfBox[index].image+" != "+getSource);
+		}
+	}
 }
--- js/scarfCon.js
+++ js/scarfCon.js
@@ -2,20 +2,20 @@ function Scarf(team, name, imageLocation) {
 	this.team = team;
 	this.scarfName = name;
 	this.image = imageLocation;
-	upVotes = 0;
+	this.upVotes = 0;
 }
 
 Scarf.prototype.upvote = function() {
-	this.upvote++;
+	this.upVotes++;
 	imgSelector(scarfBox);
 }
 
-var chainsaw = new Scarf("Timbers", "Chainsaw Border", "ptfcs1.jpg");
-var sawblade = new Scarf("Timbers", "Saw Blade", "ptfcs2.jpg");
-var tartan = new Scarf("Timbers", "The Tartan", "ptfcs3.jpg");
-var rnChamp = new Scarf("Timbers", "Champions 15 RoughNeck", "ptfc1.jpg")
-var argyle = new Scarf("Timbers", "Argyle", "ptfc2.jpg")
-var whiteGreen = new Scarf("Timbers", "White and Green", "ptfc3.jpg")
+var chainsaw = new Scarf("Timbers", "Chainsaw Border", "pics/ptfcs1.jpg");
+var sawblade = new Scarf("Timbers", "Saw Blade", "pics/ptfcs2.jpg");
+var tartan = new Scarf("Timbers", "The Tartan", "pics/ptfcs3.jpg");
+var rnChamp = new Scarf("Timbers", "Champions 15 RoughNeck", "pics/ptfc1.jpg");
+var argyle = new Scarf("Timbers", "Argyle", "pics/ptfc2.jpg");
+var whiteGreen = new Scarf("Timbers", "White and Green", "pics/ptfc3.jpg");
 
 
 var scarfBox = [];
