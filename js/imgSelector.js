249b19640c09d73544fa6116610daa584300871b
Waxhoya <waxhoya@gmail.com>
2016-07-27 18:33:43 -0700

    disable working JQuery solution

--- js/imgSelector.js
+++ js/imgSelector.js
@@ -1,10 +1,19 @@
+var picTwo = document.getElementById("secondPic");
+var picOne = document.getElementById("firstPic");
+var picThree= document.getElementById("thirdPic");
 var which = [0,1,2];
+
+picOne.addEventListener("click", scarfBox[which[0]].upvote)
+picTwo.addEventListener("click", scarfBox[which[1]].upvote)
+picThree.addEventListener("click", scarfBox[which[2]].upvote)
+
 function imgSelector(array) {
 	var picDelivered = 3; // Change the array length of pictures returned
 	which = [];
 
 	for (i = 0; i < picDelivered-1; i++){
 		var picSelect = Math.floor(Math.random() * scarfBox.length);
+		console.log("Picture Chosen:"+picSelect)
 		if (which.indexOf(picSelect)){
 			i -= 1;
 			continue;
@@ -12,15 +21,8 @@ function imgSelector(array) {
 			which.push(picSelect);
 		}
 	}
-	picOne = document.getElementById("firstPic");
-	picTwo = document.getElementById("secondPic");
-	picThree = document.getElementById("thirdPic");
-	picOne.innerHTML("<img src='"+array[which[0]].image+"' />");
-}
-picOne = document.getElementById("firstPic");
-picTwo = document.getElementById("secondPic");
-picThree = document.getElementById("thirdPic");
+	picOne.src=""+(scarfBox[which[0]].image);
+	picTwo.src=""+(scarfBox[which[1]].image);
+	picThree.src=""+(scarfBox[which[2]].image);
 
-picOne.addEventListener("click", scarfBox[which[0]].upvote)
-picTwo.addEventListener("click", scarfBox[which[1]].upvote)
-picThree.addEventListener("click", scarfBox[which[2]].upvote)
+}
