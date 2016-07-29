18591bf5748d57c641c2c2cc000dfd2e2d918c49
Waxhoya <waxhoya@gmail.com>
2016-07-29 10:59:42 -0700

    working image rotation and click

--- js/imgSelector.js
+++ js/imgSelector.js
@@ -4,17 +4,18 @@ function imgSelector(array) {
 	var picDelivered = 3; // Change the array length of pictures returned
 	which = [];
 
-	for (i = 1; i <= picDelivered; i++){
+	for (i = 1; i <= picDelivered; i++) {
 		var picSelect = Math.floor(Math.random() * scarfBox.length);
-		console.log("Picture: "+picSelect+"selected")
-		if (which.indexOf(picSelect) >= 0){
-			console.log("Picture: "+picSelect+" was already Selected");
+		var theSelection = array[picSelect].image;
+		console.log("Selected: "+theSelection+" : "+which+" Long: "+!which.indexOf(picSelect));
+		if (which.indexOf(picSelect) != -1) {
+			console.log(picSelect+"already Selected.");
 			i -= 1;
 			continue;
 		} else {
-			var theSelection = array[picSelect].image;
+
 			console.log("Attempting to push "+theSelection+" to scarfPic"+i+"!");
-			which.push(theSelection);
+			which.push(picSelect);
 			document.getElementById("scarfPic"+i).src = theSelection;
 		}
 	}
@@ -27,15 +28,15 @@ document.getElementById("scarfPic3").addEventListener("click", upvoteClick);
 
 function upvoteClick(event) {
 	var imageSource = event.target;
-	console.log(imageSource);
+//	console.log(imageSource);
 	var getSource = imageSource.src;
 	for (var index = 0; index < scarfBox.length; index++) {
-		console.log("is "+scarfBox[index].image+" = "+getSource);
+	//	console.log("is "+scarfBox[index].image+" = "+getSource);
 		if (getSource.indexOf(scarfBox[index].image) >= 0) {
-			console.log("found it: "+imageSource.src+" adding one to upvotes");
-			console.log(scarfBox[index].upVotes);
+		//	console.log("found it: "+imageSource.src+" adding one to upvotes");
+		//	console.log(scarfBox[index].upVotes);
 			scarfBox[index].upVotes++;
-			console.log(scarfBox[index].upVotes);
+		//	console.log(scarfBox[index].upVotes);
 			imgSelector(scarfBox);
 		} else {
 		//console.log(scarfBox[index].image+" != "+getSource);
