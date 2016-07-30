f299f0568fc37b2e6733333b2b7a971023dbba03
Waxhoya <waxhoya@gmail.com>
2016-07-30 15:20:36 -0700

    Added CSS to hide the scarf banners on chart load

--- js/chart.js
+++ js/chart.js
@@ -13,5 +13,8 @@ var scarfChart;
     } ]
 	});
 	scarfChart.render();
+  var flip1 = document.getElementById("scarfPic1").className = "hidden";
+  var flip2 = document.getElementById("scarfPic2").className = "hidden";
+  var flip3 = document.getElementById("scarfPic3").className = "hidden";
 }
 //window.addEventListener("load", drawChart);
